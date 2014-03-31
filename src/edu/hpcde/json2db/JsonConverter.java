13f57ebc88f0e02f252b76ecd65fe25f9773f621
robinfang <thefmajia@gmail.com>
2014-03-31 14:55:33 +0800

    可以正确存储新浪微博采集的json到数据库

--- src/edu/hpcde/json2db/JsonConverter.java
+++ src/edu/hpcde/json2db/JsonConverter.java
@@ -16,12 +16,6 @@ import java.util.Date;
 import java.util.HashMap;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,11 +28,11 @@ public class JsonConverter {
 	/**
 	 * @param args
 	 */
-	private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+	private static Logger logger = LoggerFactory.getLogger(JsonConverter.class);
 	// private JsonGenerator jsonGenerator = null;
 	private ObjectMapper objectMapper;
-	private StandardServiceRegistry serviceRegistry;
-	private static SessionFactory sessionFactory;
+	//private StandardServiceRegistry serviceRegistry;
+	//private static SessionFactory sessionFactory;
 	private static Connection con;	
 	public JsonConverter() throws Exception {
 		this.objectMapper = new ObjectMapper();
@@ -79,13 +73,14 @@ public class JsonConverter {
 	}
 	public static Connection getConnection() throws Exception{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		//logger.info("connecting");
+		logger.info("connecting");
 		String urlString = "jdbc:oracle:thin:@//222.199.193.19:1521/orcl";
 		String userString = "crawler";
 		String password = "crawler";
 		Connection con = DriverManager.getConnection(urlString, userString, password);
 		return con;
 	}
+	/*
 	public void test() {
 		Configuration configuration = new Configuration();
 		configuration.configure();
@@ -108,6 +103,12 @@ public class JsonConverter {
 		tx.commit();
 		sessionFactory.close();
 	}
+	*/
+	/**
+	 * @param username
+	 * @param userurl
+	 * @throws SQLException
+	 */
 	public void saveWeiboUser(String username, String userurl) throws SQLException{
 		String sql = "MERGE INTO weibouser w USING(SELECT ? AS n, ? AS u FROM DUAL) s ON (w.`user_url`=s.u) WHEN NOT MATCHED THEN INSERT (`user_name`,`user_url`) VALUES(s.n,s.u)".replace("`", "\"");
 		PreparedStatement pre = con.prepareStatement(sql);
@@ -116,28 +117,111 @@ public class JsonConverter {
 		pre.executeUpdate();
 		closeAll(null,pre,null);
 	} 
-	public  void saveWeiboPost(String mid, String content, String time, String userurl) throws SQLException{
-		String sql = "MERGE INTO weibopost w USING(SELECT ? AS mid, ? AS c, TO_DATE(?,'yyyyMMdd-HHmm') AS t, ? AS u FROM DUAL) s ON (w.`mid`=s.mid) WHEN NOT MATCHED THEN INSERT VALUES(S.mid,s.c,s.t,s.u)".replace("`", "\"");
+	/**
+	 * @param m
+	 * @throws SQLException
+	 */
+	public void saveWeiboUser(HashMap m) throws SQLException{
+		Object o = m.get("user_sname");
+		String username;
+		if(o!=null){
+			username = o.toString();
+		}else{
+			username = m.get("user_url").toString();
+		}
+		String user_url = m.get("user_url").toString();
+		saveWeiboUser(username, user_url);
+	}
+	/**
+	 * @param mid
+	 * @param content
+	 * @param time
+	 * @param userurl
+	 * @return
+	 * @throws SQLException
+	 */
+	public  Boolean saveWeiboPost(String mid, String content, String time, String userurl) throws SQLException{
+		String sql = "MERGE INTO weibopost w USING(SELECT ? AS mid, ? AS c, TO_DATE(?,'yyyyMMdd-HH24mi') AS t, ? AS u FROM DUAL) s ON (w.`mid`=s.mid) WHEN NOT MATCHED THEN INSERT VALUES(s.mid,s.c,s.t,s.u)".replace("`", "\"");
+		//logger.info(sql);
 		PreparedStatement pre = con.prepareStatement(sql);
 		pre.setString(1, mid);
 		pre.setString(2, content);
 		pre.setString(3, time);
 		pre.setString(4, userurl);
-		pre.executeUpdate();
+		int affectedRows = pre.executeUpdate();
+		closeAll(null,pre,null);
+		Boolean saved;
+		saved = (affectedRows>0);
+		return saved;
+	}
+
+	/**
+	 * @param time
+	 * @param content
+	 * @param repoststring
+	 * @param user_url
+	 * @param mid
+	 * @throws SQLException
+	 */
+	public void saveWeiboRepost(String time, String content, String repoststring, String user_url, String mid) throws SQLException{
+		//repostid, time, content, repoststring, user_url, mid
+		String sql = "MERGE INTO weiborepost w USING(" +
+				"select TO_DATE(?,'yyyyMMdd-HH24mi') AS time, ? AS content, ? AS repoststring, ? AS user_url, ? AS mid FROM DUAL) s " +
+				"ON (w.`user_url`=s.user_url and w.`repoststring`=s.repoststring) " +
+				"WHEN NOT MATCHED THEN INSERT (`repost_id`,`time`,`content`,`repoststring`,`user_url`,`mid`) " +
+				"VALUES(weiborepost_sequence.nextval,s.time,s.content,s.repoststring,s.user_url,s.mid)";
+		sql = sql.replace("`", "\"");
+		//logger.info(sql);
+		PreparedStatement pre = con.prepareStatement(sql);
+		pre.setString(1, time);
+		pre.setString(2, content);
+		pre.setString(3, repoststring);
+		pre.setString(4, user_url);
+		pre.setString(5, mid);
+		//logger.info("saved "+repoststring);
+		try{
+			pre.executeUpdate();
+		}catch(SQLException e){
+			logger.error("SQLException ",e);
+			logger.error("content: "+content);
+		}
 		closeAll(null,pre,null);
 	}
-	
-	public void save2DB(String filepath) throws Exception{
-			
-		File file = new File(filepath);
+	/**
+	 * @param m
+	 * @param user_url
+	 * @param mid
+	 * @throws SQLException
+	 */
+	public void saveWeiboRepost(HashMap m, String user_url, String mid) throws SQLException{
+		String time = m.get("time").toString();
+		String content = m.get("content").toString();
+		String repoststring = m.get("repost_string").toString();
+		saveWeiboRepost(time, content, repoststring, user_url, mid);
+	}
+	public void save2DB(File file) throws Exception{
+		
 		InputStream in = new FileInputStream(file);
 		HashMap<String, Object> hashmap = objectMapper.readValue(in,HashMap.class);
-		//logger.info("saving: "+hashmap.get("mid").toString());
-		ArrayList repostlist = (ArrayList) hashmap.get("repost_list");
-		logger.info(repostlist.get(0).toString());
-		//saveWeiboUser(hashmap.get("user_sname").toString(), hashmap.get("user_url").toString());
-		//saveWeiboPost(hashmap.get("mid").toString(), hashmap.get("content").toString(),hashmap.get("time").toString(),hashmap.get("userurl").toString());
-		
+		logger.info("saving: "+hashmap.get("mid").toString());
+		String mid = hashmap.get("mid").toString();
+		saveWeiboUser(hashmap.get("user_sname").toString(), hashmap.get("user_url").toString());
+		Boolean saved = saveWeiboPost(hashmap.get("mid").toString(), hashmap.get("content").toString(),hashmap.get("time").toString(),hashmap.get("user_url").toString());
+		if(saved){
+			ArrayList repostlist = (ArrayList) hashmap.get("repost_list");
+			for(Object item : repostlist){
+				HashMap m = (HashMap)item;
+				String user_url = m.get("user_url").toString();
+				saveWeiboUser(m);
+				saveWeiboRepost(m, user_url, mid);
+			}
+		}else{
+			logger.info("mid "+mid+" exists");
+		}
+	}
+	public void save2DB(String filepath) throws Exception{
+		File file = new File(filepath);
+		save2DB(file);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -146,8 +230,13 @@ public class JsonConverter {
 		// InputStream in = new FileInputStream(file);
 		// JsonConverter j = new JsonConverter();
 		// j.readJson2Map(in);
+		File dir = new File("E:\\pyworkspace\\weibo_3_20_test");
+		File[] files = dir.listFiles();
 		JsonConverter jc = new JsonConverter();
-		jc.save2DB("E:/fworkspace/Ah8KZjeYu.json");
+		for(File file:files){
+			jc.save2DB(file);
+		}
+		//jc.save2DB("e:/workspace/A0ju7ih4D.json");
 		jc.destory();
 	}
 
